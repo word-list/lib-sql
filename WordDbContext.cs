05ee688ae555a0d6268eed039ce3feed05fc2ef6
Jonathan Gaul <jon@gaul.tech>
2025-06-08 09:11:15 +0000

    Add default constructor to context

--- WordDbContext.cs
+++ WordDbContext.cs
@@ -13,11 +13,18 @@ public class WordDbContext : DbContext
     public DbSet<Word> Words { get; set; }
     public DbSet<WordType> Types { get; set; }
 
-    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "This will not be used with AOT")]
-    [UnconditionalSuppressMessage("AOT", "IL3050:Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.", Justification = "Will not be used with AOT")]
+    [RequiresUnreferencedCode("")]
+    [RequiresDynamicCode("")]
     public WordDbContext(DbContextOptions options)
         : base(options)
     {
+    }
+
+    [RequiresUnreferencedCode("")]
+    [RequiresDynamicCode("")]
+    public WordDbContext() : base()
+    {
+
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
