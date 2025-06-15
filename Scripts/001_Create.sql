53da112d3f323a954886cfcc1341ef324a0d456d
Jonathan Gaul <jon@gaul.tech>
2025-06-15 11:59:31 +0000

    Update apply migrations workflow and add new word attribute fields

--- Scripts/001_Create.sql
+++ Scripts/001_Create.sql
@@ -1,15 +1,15 @@
-CREATE TABLE words (
+CREATE TABLE IF NOT EXISTS words (
     text STRING UNIQUE PRIMARY KEY,
     commonness INT,
     offensiveness INT,
     sentiment INT
 );
 
-CREATE TABLE word_types (
+CREATE TABLE IF NOT EXISTS word_types (
     name STRING UNIQUE PRIMARY KEY
 );
 
-CREATE TABLE word_word_types (
+CREATE TABLE IF NOT EXISTS word_word_types (
     word_text STRING REFERENCES words(text),
     word_type_name STRING REFERENCES word_types(name),
     PRIMARY KEY (word_text, word_type_name)
